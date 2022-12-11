bd4372dd164483f81f10fd733af8eac92a391cac
Jack Dockerty <jdockerty19@gmail.com>
2022-12-11 16:57:38 +0000

    wip: add Stream type and corresponding funcs

--- stream.go
+++ stream.go
@@ -1,7 +1,70 @@
 package yamlstream
 
-import "fmt"
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+)
 
-func Hello() {
-	fmt.Println("Hello world!")
+type Stream struct {
+	Stream [][]byte
+	Count  int
+	B      [][]byte
+}
+
+func (s *Stream) Bytes() []byte {
+    b := &bytes.Buffer{}
+    for _, data := range s.Stream {
+        b.Write(data)
+    }
+
+	return b.Bytes() 
+}
+
+func (s *Stream) String() string {
+    return string(s.Bytes())
+}
+
+func (s *Stream) Read(r io.Reader) error {
+	rd := bufio.NewReader(r)
+
+    // Initialise count as 1 when passed a Reader, this covers
+    // the case where there are no delimiters since this is
+    // considered a "stream", i.e. a single file.
+    s.Count = 1
+    
+	b := &bytes.Buffer{}
+	for {
+		line, err := rd.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return fmt.Errorf("unable to read: %w", err)
+		}
+
+		if bytes.Equal(line, []byte("---\n")) {
+			s.Count += 1
+            s.Stream = append(s.Stream, b.Bytes())
+
+            // Clear buffer for next delimiter
+			b = &bytes.Buffer{}
+		}
+
+		_, err = b.Write(line)
+		if err != nil {
+			return fmt.Errorf("unable to write data: %w", err)
+		}
+
+	}
+
+	return nil
+
+}
+
+func New() *Stream {
+	return &Stream{
+		Stream: nil,
+	}
 }
--- stream_test.go
+++ stream_test.go
@@ -1,11 +1,63 @@
 package yamlstream_test
 
 import (
+	"bytes"
+	"os"
 	"testing"
 
 	yamlstream "github.com/jdockerty/yaml-stream"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHello(t *testing.T) {
-	yamlstream.Hello()
+const (
+	simpleYamlStream = "testdata/simple_stream.yaml"
+)
+
+func TestNewWithDefaults(t *testing.T) {
+
+	ys := yamlstream.New()
+
+	assert.IsType(t, ys, &yamlstream.Stream{})
+}
+
+func TestStreamCounter(t *testing.T) {
+	ys := yamlstream.New()
+
+	f, _ := os.Open(simpleYamlStream)
+	defer f.Close()
+
+	err := ys.Read(f)
+	assert.Nil(t, err)
+
+	assert.Equal(t, 3, ys.Count)
+}
+
+func TestBytes(t *testing.T) {
+
+	ys := yamlstream.New()
+
+	f, _ := os.Open(simpleYamlStream)
+	defer f.Close()
+
+	_ = ys.Read(f)
+
+	assert.IsType(t, ys.Bytes(), make([]byte, 0))
+}
+
+func TestReadBytesEquality(t *testing.T) {
+
+	ys := yamlstream.New()
+
+	f, _ := os.Open(simpleYamlStream)
+	defer f.Close()
+
+	err := ys.Read(f)
+	assert.Nil(t, err)
+
+	fileAsBytes, _ := os.ReadFile(simpleYamlStream)
+
+    t.Logf("AsBytes: %s\n", ys.Bytes())
+    t.Logf("\nGot:%s\nWant:%s", ys.String(), string(fileAsBytes))
+	assert.True(t, bytes.Equal(ys.Bytes(), fileAsBytes))
+
 }
